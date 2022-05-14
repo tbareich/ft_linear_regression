92f99757e0e958b877f4ba2fa4300551879a0c6d
tbareich <tareqbareich@gmail.com>
2022-05-14 14:04:16 +0100

    fix predict function when the mileage is not a number.

--- predict.py
+++ predict.py
@@ -1,6 +1,17 @@
 from genericpath import exists
 from model import f
 
+
+def get_mileage(theta_0, theta_1):
+    try:
+        mileage_arg = input("Tap a car mileage: ")
+        mileage = float(mileage_arg)
+        print(f(mileage, theta_0, theta_1))
+    except ValueError as e:
+        print('Enter a valid number.')
+        get_mileage(theta_0, theta_1)
+
+
 try:
     theta_0 = 0
     theta_1 = 0
@@ -9,8 +20,6 @@ try:
         weights = file.readline().split(' ')
         theta_0 = float(weights[0])
         theta_1 = float(weights[1])
-    mileage_arg = input("Tap a car mileage: \n")
-    mileage = float(mileage_arg)
-    print(f(mileage, theta_0, theta_1))
+    get_mileage(theta_0, theta_1)
 except Exception as e:
     print('Something went wrong.')
